7908e9c9525c62344c89771cb5c303a18ec1554b
Luiz Araujo <arauldesign@gmail.com>
2024-04-25 17:32:33 -0300

    feature: creating controller and actions - projects

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,57 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            // Busca todos ou filtra
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            // Busca o projeto
+
+            // return NotFound();
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel createProject) 
+        { 
+            if (createProject.Title.Length > 50)
+                return BadRequest();
+
+            // Cadastra o projeto
+
+            return CreatedAtAction(nameof(GetById), new { id = createProject.Id }, createProject);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody] UpdateProjectModel updateProject)
+        {
+            if (updateProject.Description.Length > 200)
+                return BadRequest();
+
+            // Atualizo o objeto
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id) 
+        {
+            // Busca se não existir retorna not found
+
+            // Remover
+
+            return NoContent();
+        }
+    }
+}
