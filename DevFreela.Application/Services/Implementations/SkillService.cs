f920dc10f208209a53ce67124471f9e688d6cda1
Luiz Araujo <arauldesign@gmail.com>
2024-04-29 14:24:34 -0300

    feature: implementing skills service

---
+++ DevFreela.Application/Services/Implementations/SkillService.cs
@@ -0,0 +1,26 @@
+﻿using DevFreela.Application.ViewModels;
+using DevFreela.Infrastructure.Persistence;
+using DevFreela.Application.Services.Interfaces;
+
+namespace DevFreela.Application.Services.Implementations
+{
+    public class SkillService : ISkillService
+    {
+        private readonly DevFreelaDbContext _dbContext;
+        public SkillService(DevFreelaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<SkillViewModel> GetAll()
+        {
+            var skills = _dbContext.Skills;
+
+            var skilldViewModel = skills
+                .Select(s => new SkillViewModel(s.Id, s.Description))
+                .ToList();
+
+            return skilldViewModel;
+        }
+    }
+}
