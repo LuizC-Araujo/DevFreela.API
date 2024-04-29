cca20c522cadb015dc511a214d0a73b3061ebb4d
Luiz Araujo <arauldesign@gmail.com>
2024-04-29 13:30:07 -0300

    feature: creating inputModels, viewModel and Sevices to Project

---
+++ DevFreela.Application/Services/Interfaces/IProjectService.cs
@@ -0,0 +1,18 @@
+﻿using DevFreela.Application.InputModels;
+using DevFreela.Application.ViewModels;
+
+namespace DevFreela.Application.Services.Interfaces
+{
+    public interface IProjectService
+    {
+        List<ProjectViewModel> GetAll(string query);
+        ProjectDetailsViewModel GetById(int id);
+        int Create(NewProjectInputModel inputModel);
+        void Update(UpdateProjectInputModel inputModel);
+        void createComment(CreateCommandInputModel inputmModel);
+        void Delete(int id);
+        void Start(int id);
+        void Finished(int id);
+
+    }
+}
