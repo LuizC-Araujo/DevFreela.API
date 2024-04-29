62c7bc8b65e83fe093ab7c8a5140f63b108f2846
Luiz Araujo <arauldesign@gmail.com>
2024-04-29 14:23:59 -0300

    feature: adding id na project viewmodel

--- DevFreela.Application/Services/Implementations/ProjectService.cs
+++ DevFreela.Application/Services/Implementations/ProjectService.cs
@@ -49,7 +49,7 @@ namespace DevFreela.Application.Services.Implementations
             var projects = _dbContext.Projects;
 
             var projectsViewModel = projects
-                .Select(x => new ProjectViewModel(x.Title, x.CreatedAt))
+                .Select(x => new ProjectViewModel(x.Id, x.Title, x.CreatedAt))
                 .ToList();
 
             return projectsViewModel;
