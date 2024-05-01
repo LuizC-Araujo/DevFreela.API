b3253290dbf4c705037d8c67e068fb1bae1a0fc6
Luiz Araujo <arauldesign@gmail.com>
2024-05-01 16:40:18 -0300

    feature: configuring entities to tables and relationships

--- DevFreela.Core/Entities/User.cs
+++ DevFreela.Core/Entities/User.cs
@@ -29,5 +29,6 @@ namespace DevFreela.Core.Entities
         public List<UserSkill> Skills { get; private set; }
         public List<Project> OwnedProjects { get; private set; }
         public List<Project> FreelanceProjects { get; private set; }
+        public List<ProjectComment> Comments { get; set; }
     }
 }
