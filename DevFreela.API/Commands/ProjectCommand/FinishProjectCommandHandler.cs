d1767abb1bacd0ba49554c83681bf2a388e73f9f
Luiz Araujo <arauldesign@gmail.com>
2024-05-05 18:36:51 -0300

    feature: implementing finish project commands

---
+++ DevFreela.API/Commands/ProjectCommand/FinishProjectCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevFreela.API.Commands.ProjectCommand
+{
+    public class FinishProjectCommandHandler : IRequestHandler<FinishProjectCommand, int>
+    {
+        private readonly DevFreelaDbContext _dbContext;
+        public FinishProjectCommandHandler(DevFreelaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<int> Handle(FinishProjectCommand request, CancellationToken cancellationToken)
+        {
+            var project = await _dbContext.Projects.FirstOrDefaultAsync(x => x.Id == request.Id);
+
+            if (project == null) return 0;
+
+            project.Finish();
+            await _dbContext.SaveChangesAsync();
+
+            return project.Id;
+        }
+    }
+}
