7e3370d47d4436f6cbcbf2002984e11110c5a69e
Luiz Araujo <arauldesign@gmail.com>
2024-05-05 18:43:00 -0300

    feature: implementing start project commands

---
+++ DevFreela.API/Commands/ProjectCommand/StartProjectCommand.cs
@@ -0,0 +1,11 @@
+﻿using DevFreela.Core.Enums;
+using MediatR;
+
+namespace DevFreela.API.Commands.ProjectCommand
+{
+    public class StartProjectCommand : IRequest<int>
+    {
+        public int Id { get; set; }
+        public EProjectStatusEnum Status { get; set; }
+    }
+}
---
+++ DevFreela.API/Commands/ProjectCommand/StartProjectCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevFreela.API.Commands.ProjectCommand
+{
+    public class StartProjectCommandHandler : IRequestHandler<StartProjectCommand, int>
+    {
+        private readonly DevFreelaDbContext _dbContext;
+        public StartProjectCommandHandler(DevFreelaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public  async Task<int> Handle(StartProjectCommand request, CancellationToken cancellationToken)
+        {
+            var project = await _dbContext.Projects.FirstOrDefaultAsync(x => x.Id == request.Id);
+
+            if (project == null) return 0;
+
+            project.Start();
+            await _dbContext.SaveChangesAsync();
+
+            return project.Id;
+        }
+    }
+}
