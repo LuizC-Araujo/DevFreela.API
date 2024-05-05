063a49194f71e3441ad122313892845218de63ad
Luiz Araujo <arauldesign@gmail.com>
2024-05-05 18:23:34 -0300

    feature: command to cancel project

---
+++ DevFreela.API/Commands/ProjectCommand/CancelProjectCommand.cs
@@ -0,0 +1,11 @@
+﻿using DevFreela.Core.Enums;
+using MediatR;
+
+namespace DevFreela.API.Commands.ProjectCommand
+{
+    public class CancelProjectCommand : IRequest<int>
+    {
+        public int Id { get; set; }
+        public ProjectStatusEnum Status { get; set; }
+    }
+}
---
+++ DevFreela.API/Commands/ProjectCommand/CancelProjectCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevFreela.API.Commands.ProjectCommand
+{
+    public class CancelProjectCommandHandler : IRequestHandler<CancelProjectCommand, int>
+    {
+        private readonly DevFreelaDbContext _dbContext;
+        public CancelProjectCommandHandler(DevFreelaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<int> Handle(CancelProjectCommand request, CancellationToken cancellationToken)
+        {
+            var project = await _dbContext.Projects.FirstOrDefaultAsync(x => x.Id == request.Id);
+
+            if (project == null) return 0;
+
+            project.Cancel();
+            await _dbContext.SaveChangesAsync();
+
+            return project.Id;
+        }
+    }
+}
