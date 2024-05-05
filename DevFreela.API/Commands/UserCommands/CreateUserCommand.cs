f9be6c980cdb31683da335dd5a8675c511191374
Luiz Araujo <arauldesign@gmail.com>
2024-05-05 19:41:22 -0300

    feature: implementing user create commands

---
+++ DevFreela.API/Commands/UserCommands/CreateUserCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace DevFreela.API.Commands.UserCommands
+{
+    public class CreateUserCommand : IRequest<int>
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
