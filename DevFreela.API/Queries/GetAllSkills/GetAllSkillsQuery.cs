f20b25abec81d56577a091f774d3408736b9f0d5
Luiz Araujo <arauldesign@gmail.com>
2024-05-07 13:17:51 -0300

    feature: implementing skills query

---
+++ DevFreela.API/Queries/GetAllSkills/GetAllSkillsQuery.cs
@@ -0,0 +1,9 @@
+﻿using DevFreela.Application.ViewModels;
+using MediatR;
+
+namespace DevFreela.API.Queries.GetAllSkills
+{
+    public class GetAllSkillsQuery : IRequest<List<SkillViewModel>>
+    {
+    }
+}
---
+++ DevFreela.API/Queries/GetAllSkills/GetAllSkillsQueryHandler.cs
@@ -0,0 +1,49 @@
+﻿using DevFreela.Application.ViewModels;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+
+namespace DevFreela.API.Queries.GetAllSkills
+{
+    public class GetAllSkillsQueryHandler : IRequestHandler<GetAllSkillsQuery, List<SkillViewModel>>
+    {
+
+        //using dapper
+        //private readonly string _connectionString;
+        //public GetAllSkillsQueryHandler(IConfiguration configuration)
+        //{
+        //    _connectionString = configuration.GetConnectionString("DevFreelaCs");
+        //}
+
+        //public async Task<List<SkillViewModel>> Handle(GetAllSkillsQuery request, CancellationToken cancellationToken)
+        //{
+        //    using (var sqlConnection = new SqlConnection(_connectionString))
+        //    {
+        //        sqlConnection.Open();
+
+        //        var script = "SELECT Id, Desciption FROM Skills";
+
+        //        var skills = await sqlConnection.QueryAsync<SkillViewModel>(script);
+
+        //        return skills.ToList();
+        //    }
+        //}
+
+        //using EF Core
+        private readonly DevFreelaDbContext _dbContext;
+        public GetAllSkillsQueryHandler(DevFreelaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<SkillViewModel>> Handle(GetAllSkillsQuery request, CancellationToken cancellationToken)
+        {
+            var skills = _dbContext.Skills;
+
+            var skilldViewModel = skills
+                .Select(s => new SkillViewModel(s.Id, s.Description))
+                .ToList();
+
+            return skilldViewModel;
+        }
+    }
+}
