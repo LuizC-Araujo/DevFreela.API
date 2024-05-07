4b152995f041dcc5d9d55543093d9f0abe21fb98
Luiz Araujo <arauldesign@gmail.com>
2024-05-07 13:25:37 -0300

    feature: adding skill controller

---
+++ DevFreela.API/Controllers/SkillController.cs
@@ -0,0 +1,26 @@
+﻿using DevFreela.API.Queries.GetAllSkills;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/skills")]
+    public class SkillController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public SkillController(IMediator mediator)
+        {
+             _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var query = new GetAllSkillsQuery();
+
+            var skills = await _mediator.Send(query);
+
+            return Ok(skills);
+        }
+    }
+}
