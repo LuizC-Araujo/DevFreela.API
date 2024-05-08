93f4d23144cce48e220be350dab2e42fc371360c
Luiz Araujo <arauldesign@gmail.com>
2024-05-08 16:37:03 -0300

    feature: implementing repository query get by id

--- DevFreela.Core/Repositories/IProjectRepository.cs
+++ DevFreela.Core/Repositories/IProjectRepository.cs
@@ -4,6 +4,7 @@ namespace DevFreela.Core.Repositories
 {
     public interface IProjectRepository
     {
-        Task<List<Project>> GetAll();
+        Task<List<Project>> GetAllAsync();
+        Task<Project> GetByIdAsync(int id);
     }
 }
