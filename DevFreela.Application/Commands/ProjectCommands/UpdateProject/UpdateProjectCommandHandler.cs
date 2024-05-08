307954bc612f98763b0d9ab6d719587145b3f469
Luiz Araujo <arauldesign@gmail.com>
2024-05-08 17:16:40 -0300

    feature: implementing repository pattern to update command

--- DevFreela.Application/Commands/ProjectCommands/UpdateProject/UpdateProjectCommandHandler.cs
+++ DevFreela.Application/Commands/ProjectCommands/UpdateProject/UpdateProjectCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using DevFreela.Infrastructure.Persistence;
+﻿using DevFreela.Core.Repositories;
+using DevFreela.Infrastructure.Persistence;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,20 +7,20 @@ namespace DevFreela.Application.Commands.ProjectCommands.UpdateProject
 {
     public class UpdateProjectCommandHandler : IRequestHandler<UpdateProjectCommand, Unit>
     {
-        private readonly DevFreelaDbContext _dbContext;
-        public UpdateProjectCommandHandler(DevFreelaDbContext dbContext)
+        private readonly IProjectRepository _projectRepository;
+        public UpdateProjectCommandHandler(IProjectRepository projectRepository)
         {
-            _dbContext = dbContext;
+            _projectRepository = projectRepository;
         }
         public async Task<Unit> Handle(UpdateProjectCommand request, CancellationToken cancellationToken)
         {
-            var project = await _dbContext.Projects.SingleOrDefaultAsync(x => x.Id == request.Id);
+            var project = await _projectRepository.GetByIdAsync(request.Id);
 
             if (project == null) return Unit.Value;
 
             project.Update(request.Title, request.Description, request.TotalCost);
 
-            await _dbContext.SaveChangesAsync();
+            await _projectRepository.SaveChangesAsync();
 
             return Unit.Value;
         }
