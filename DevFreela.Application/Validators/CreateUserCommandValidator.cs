b0c96fed6e1b880323a670bbf0778beead5aafc5
Luiz Araujo <arauldesign@gmail.com>
2024-05-09 16:13:18 -0300

    feature: implementing creste user validation

---
+++ DevFreela.Application/Validators/CreateUserCommandValidator.cs
@@ -0,0 +1,30 @@
+﻿using DevFreela.Application.Commands.UserCommands;
+using FluentValidation;
+using System.Text.RegularExpressions;
+
+namespace DevFreela.Application.Validators
+{
+    public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
+    {
+        public CreateUserCommandValidator()
+        {
+            RuleFor(x => x.Email)
+                .EmailAddress().WithMessage("E-mail inválido.");
+
+            RuleFor(x => x.Password)
+                .Must(ValidPassword)
+                .WithMessage("Senha deve conter pelo menos 8 caracteres, um número, uma letra  maiúscula e um caractere especial");
+
+            RuleFor(x => x.FullName)
+                .NotEmpty()
+                .NotNull().WithMessage("O nome é obrigatório!");
+        }
+
+        private bool ValidPassword(string password)
+        {
+            var regex = new Regex(@"^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%^&+=].*$)");
+
+            return regex.IsMatch(password);
+        }
+    }
+}
