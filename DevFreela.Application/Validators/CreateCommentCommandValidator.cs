0e4ec5a6eb1dcc55f90e11b1f71c9bd750cea957
Luiz Araujo <arauldesign@gmail.com>
2024-05-10 10:39:29 -0300

    feature: implementing validations to create comment command

---
+++ DevFreela.Application/Validators/CreateCommentCommandValidator.cs
@@ -0,0 +1,26 @@
+﻿using DevFreela.Application.Commands.ProjectCommands.CreateComment;
+using FluentValidation;
+
+namespace DevFreela.Application.Validators
+{
+    public class CreateCommentCommandValidator : AbstractValidator<CreateCommentCommand>
+    {
+        public CreateCommentCommandValidator()
+        {
+            RuleFor(c => c.Content)
+                .NotEmpty()
+                .NotNull().WithMessage("O conteúdo deve ser preenchido!")
+                .MaximumLength(255).WithMessage("O tamanho máximo do conteúdo é de 255 carateres!");
+
+            RuleFor(c => c.IdUser)
+                .NotEmpty()
+                .NotNull().WithMessage("O id do usuário deve ser preenchido")
+                .GreaterThan(0).WithMessage("O id do usuário deve ser mair que 0");
+
+            RuleFor(c => c.IdProject)
+                .NotEmpty()
+                .NotNull().WithMessage("O id do projeto deve ser preenchido")
+                .GreaterThan(0).WithMessage("O id do projeto deve ser mair que 0");
+        }
+    }
+}
