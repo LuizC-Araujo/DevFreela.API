0d63f083b2bbe9e594b396152944936bc840dc96
Luiz Araujo <arauldesign@gmail.com>
2024-05-14 20:24:55 -0300

    feature: creating get all projects handler teste using given-when-then pattern

---
+++ DevFreela.UnitTests/Application/Commands/GetAllProjectsCommandHandlerTests.cs
@@ -0,0 +1,18 @@
+﻿using DevFreela.Application.Queries.GetAllProjects;
+using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using Moq;
+using System.Net.WebSockets;
+using Xunit;
+
+namespace DevFreela.UnitTests.Application.Commands
+{
+    public class GetAllProjectsCommandHandlerTests
+    {
+        [Fact]
+        public async Task ThreeProjectsExist_Executed_ReturnThreeProjectViewModels()
+        {
+
+        }
+    }
+}
