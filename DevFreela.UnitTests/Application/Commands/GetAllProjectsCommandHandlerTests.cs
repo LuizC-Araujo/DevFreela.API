03924f69168a5f5227039920ab7f027f80e450fc
Luiz Araujo <arauldesign@gmail.com>
2024-05-14 20:25:44 -0300

    feature: using aaa pattern, arrange

--- DevFreela.UnitTests/Application/Commands/GetAllProjectsCommandHandlerTests.cs
+++ DevFreela.UnitTests/Application/Commands/GetAllProjectsCommandHandlerTests.cs
@@ -12,6 +12,24 @@ namespace DevFreela.UnitTests.Application.Commands
         [Fact]
         public async Task ThreeProjectsExist_Executed_ReturnThreeProjectViewModels()
         {
+            // arrange
+            var projects = new List<Project>
+            { 
+                new("Nome do Teste 1", "Descrição 1", 1, 2, 10000),
+                new("Nome do Teste 2", "Descrição 3", 1, 2, 20000),
+                new("Nome do Teste 3", "Descrição 4", 1, 2, 30000)
+            };
+
+            var projectRepository = new Mock<IProjectRepository>();
+            projectRepository.Setup(pr => pr.GetAllAsync().Result).Returns(projects);
+
+            var getAllProjectsQuery = new GetAllProjectsQuery();
+            var getAllProjectsQueryHandler = new GetAllProjectsQueryHandler(projectRepository.Object);
+
+            //act
+           
+
+            //assert
 
         }
     }
