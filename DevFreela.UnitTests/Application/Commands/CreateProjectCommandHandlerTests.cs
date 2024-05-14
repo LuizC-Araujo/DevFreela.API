9d9ac3c3c4e33f620827296fdcba718246fbf393
Luiz Araujo <arauldesign@gmail.com>
2024-05-14 20:36:37 -0300

    feature: creating create project command handler tests with given when then pattern

---
+++ DevFreela.UnitTests/Application/Commands/CreateProjectCommandHandlerTests.cs
@@ -0,0 +1,11 @@
+﻿namespace DevFreela.UnitTests.Application.Commands
+{
+    public class CreateProjectCommandHandlerTests
+    {
+        [Fact]
+        public async Task InputaDataIsOk_Executed_ReturnProjectId()
+        {
+
+        }
+    }
+}
