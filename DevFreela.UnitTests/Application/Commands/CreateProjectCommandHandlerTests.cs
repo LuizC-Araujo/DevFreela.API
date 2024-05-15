48aeae6d7f371a73fd96e8e7ed66221d1d03efd3
Luiz Araujo <arauldesign@gmail.com>
2024-05-15 16:26:39 -0300

    feature: implementing aaa pattern, arrange

--- DevFreela.UnitTests/Application/Commands/CreateProjectCommandHandlerTests.cs
+++ DevFreela.UnitTests/Application/Commands/CreateProjectCommandHandlerTests.cs
@@ -1,11 +1,31 @@
-﻿namespace DevFreela.UnitTests.Application.Commands
+﻿using DevFreela.Application.Commands.ProjectCommands.CreateProject;
+using DevFreela.Core.Repositories;
+using Moq;
+
+namespace DevFreela.UnitTests.Application.Commands
 {
     public class CreateProjectCommandHandlerTests
     {
         [Fact]
         public async Task InputaDataIsOk_Executed_ReturnProjectId()
         {
+            // arrange
+            var projectRepository = new Mock<IProjectRepository>();
 
+            var createProjectCommand = new CreateProjectCommand
+            {
+                Title = "Title Test",
+                Description = "Description Test",
+                IdClient = 1,
+                IdFreelancer = 2,
+                TotalCost = 5000
+            };
+
+            var createProjectCommandHandler = new CreateProjectCommandHandler(projectRepository.Object);
+
+            // act
+
+            // assert
         }
     }
 }
